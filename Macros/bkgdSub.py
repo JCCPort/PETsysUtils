b0812a573601bc3db1d7a8f21d702f928fe97580
Jess Lock <jl831@sussex.ac.uk>
2022-07-06 15:38:15 +0100

    formatting

--- Macros/bkgdSub.py
+++ Macros/bkgdSub.py
@@ -14,18 +14,20 @@ parser.add_argument("--range", dest='range', type=float, nargs='*', default=[-10
                     help='Range to plot over. If not included, will plot between [-10, 500]')
 parser.add_argument("--bins", dest='bins', type=float, nargs='*', default=10000,
                     help='Number of bins. If not included bins=10000')
+
 args = parser.parse_args()
 
-sFile = args.signalName + ".ldat"  # binary file containing data
-bFile = args.bkgdName + ".ldat"  # binary file containing data
+# set as binary files
+sFile = args.signalName + ".ldat"
+bFile = args.bkgdName + ".ldat"
 
 # set dataframes
-dtS = np.dtype([('time', np.int64), ('energy', np.float32), ('channelID', np.int32)])
-dataSignal = np.fromfile(sFile, dtype=dtS)
+dt = np.dtype([('time', np.int64), ('energy', np.float32), ('channelID', np.int32)])
+
+dataSignal = np.fromfile(sFile, dtype=dt)
 dataSignal = pd.DataFrame(dataSignal)
 
-dtB = np.dtype([('time', np.int64), ('energy', np.float32), ('channelID', np.int32)])
-dataBkgd = np.fromfile(bFile, dtype=dtB)
+dataBkgd = np.fromfile(bFile, dtype=dt)
 dataBkgd = pd.DataFrame(dataBkgd)
 
 if args.channel:
@@ -33,22 +35,21 @@ if args.channel:
     dataBkgd = dataBkgd.loc[dataBkgd['channelID'] == args.channel]
 
 # extract energy data
-
 energySignal = dataSignal['energy']
 energyBkgd = dataBkgd['energy']
 
 # make histograms
-histS, edgesS = np.histogram(energySignal, bins=np.linspace(0,50,300))
+histS, edgesS = np.histogram(energySignal, bins=np.linspace(0, 50, 300))
 leftEdgesS = edgesS[:-1]
-width = 1*(leftEdgesS[1] - leftEdgesS[0])
+width = 1 * (leftEdgesS[1] - leftEdgesS[0])
 
-histB, edgesB = np.histogram(energyBkgd, bins=np.linspace(0,50,300))
+histB, edgesB = np.histogram(energyBkgd, bins=np.linspace(0, 50, 300))
 leftEdgesB = edgesB[:-1]
 
 # subtract histograms
 histSub = histS - histB
 
-plt.bar(leftEdgesS, histSub, align='edge',width=width)
+plt.bar(leftEdgesS, histSub, align='edge', width=width)
 plt.title("Background subtracted")
 plt.show()
 
@@ -61,4 +62,3 @@ plt.title("Background")
 plt.show()
 print(len(energySignal))
 print(len(energyBkgd))
-
