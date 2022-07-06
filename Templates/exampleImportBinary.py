327c9b21eb9640b1e01358ac7b28b9b778ab3803
Jess Lock <jl831@sussex.ac.uk>
2022-07-06 15:32:40 +0100

    formatting

--- Templates/exampleImportBinary.py
+++ Templates/exampleImportBinary.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import argparse
 
-
+# read in data
 parser = argparse.ArgumentParser(description='Read petsys binary files')
 
 parser.add_argument("-o", dest='fileName', type=str, required=True, help='Prefix filename of signal binary file')
@@ -14,7 +14,7 @@ parser.add_argument("--channel", dest='channel', type=int, help='Channel to look
 args = parser.parse_args()
 file = args.fileName + ".ldat"
 
-# set dataframes
+# turn into pandas dataframe
 dt = np.dtype([('time', np.int64), ('energy', np.float32), ('channelID', np.int32)])
 data = np.fromfile(file, dtype=dt)
 data = pd.DataFrame(data)
