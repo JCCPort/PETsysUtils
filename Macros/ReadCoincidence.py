11110e59d5ad97b578ca764e6a93a0b429d90c68
petsys <petsys@localhost.localdomain>
2022-07-29 16:39:31 +0100

    Macro change

--- Macros/ReadCoincidence.py
+++ Macros/ReadCoincidence.py
@@ -5,19 +5,23 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    data = np.fromfile(sys.argv[0], dtype=[("time", np.longlong),
+    data = np.fromfile(sys.argv[1], dtype=[("time", np.longlong),
                                                                                                ("energy", np.single),
                                                                                                ("channel", np.intc),
                                                                                                ("group", np.int64)])
 
     data = pd.DataFrame(data)
+    #print(data[0:30])
     groupSumEnergies = data.groupby('group')['energy'].sum().reset_index()['energy'].values
-    groupSumEnergies = groupSumEnergies[groupSumEnergies < 1e4]
-    plt.hist(groupSumEnergies, bins=300, label="80 ns window, majority 2")
-    # plt.xlim([0, 200])
+    print(len(groupSumEnergies))
+    #print(np.min(groupSumEnergies), np.max(groupSumEnergies))
+    #print(groupSumEnergies)
+    # groupSumEnergies = groupSumEnergies[groupSumEnergies < 1e4]
+    plt.hist(groupSumEnergies, bins=500, label="80 ns window, majority 5")
+    #plt.xlim([-2, 20])
     plt.ylabel("Counts")
     plt.xlabel("Sum energy")
-    plt.title("run5_LED_qdc_single_sorted_grouped.ldat")
+    plt.title(sys.argv[1].split("/")[-1])
     plt.legend()
     plt.tight_layout()
     plt.show()
