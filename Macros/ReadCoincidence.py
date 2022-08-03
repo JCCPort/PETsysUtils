4c8c9ef41767f7256c8e2a492b24e3c6ce29d6d2
Jess Lock <jl831@sussex.ac.uk>
2022-08-03 17:31:53 +0100

    Add label and debug at cmd line functionality

--- Macros/ReadCoincidence.py
+++ Macros/ReadCoincidence.py
@@ -5,22 +5,33 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    data = np.fromfile(sys.argv[1], dtype=[("time", np.longlong),
-                                                                                               ("energy", np.single),
-                                                                                               ("channel", np.intc),
-                                                                                               ("group", np.int64)])
+    data = np.fromfile(sys.argv[1],
+                       dtype=[("time", np.longlong), ("energy", np.single), ("channel", np.intc), ("group", np.int64)])
 
+    if len(sys.argv) == 2:
+        histLabel = None
+    else:
+        histLabel = "{}".format(sys.argv[2])
+        print("Argument after file path reserved for histogram label or 'debug'")
+
+    # Each coincidence group has energies summed
     data = pd.DataFrame(data)
-    #print(data[0:30])
     groupSumEnergies = data.groupby('group')['energy'].sum().reset_index()['energy'].values
-    print(len(groupSumEnergies))
-    #print(np.min(groupSumEnergies), np.max(groupSumEnergies))
-    #print(groupSumEnergies)
+
     # groupSumEnergies = groupSumEnergies[groupSumEnergies < 1e4]
-    plt.hist(groupSumEnergies, bins=500, label="80 ns window, majority 5")
-    #plt.xlim([-2, 20])
+
+    # Data debug
+    if sys.argv[2] == "debug":
+        print("DEBUG")
+        print("First 10 entries: \n", data[0:10])
+        print("Length of data:", len(data))
+        print("Length of groupSumEnergies:", len(groupSumEnergies))
+        print("groupSumEnergies min = {}, max = {}".format(np.min(groupSumEnergies), np.max(groupSumEnergies)))
+
+    plt.hist(groupSumEnergies, bins=500, label=histLabel)
+    # plt.xlim([-2, 20])
     plt.ylabel("Counts")
-    plt.xlabel("Sum energy")
+    plt.xlabel("Sum Energy")
     plt.title(sys.argv[1].split("/")[-1])
     plt.legend()
     plt.tight_layout()
